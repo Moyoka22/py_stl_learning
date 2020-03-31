1c9e472bc9556ec229a7051c2cca17de7b7ee085
Moyewa Odiase <moyeodiase@hotmail.co.uk>
2020-03-31 10:53:46 +0100

    Additional example showing slicing notation is only a shallow copy

--- memory/mutable_and_immutable.py
+++ memory/mutable_and_immutable.py
@@ -24,27 +24,57 @@ def afunc2(list_param : list, tuple_param : tuple):
     logging.info(f'The id of l is: {id(list_param)}')
     logging.info(f'The id of t is: {id(tuple_param)}')
 
+def afunc3(list_param : list, tuple_param : tuple):
+    logging.info(f'The id of l2 is: {id(list_param)}')
+    list_param[1].append(6) # * The name points to the same object as in the calling scope thus calling append changes the list
+    # ! Tuples are immutable, they do not have any methods which can alter their contents
+    # ! and so the object bound to in the calling scope cannot be changed in function scope
+    logging.info(f'The id of l2 is: {id(list_param)}')
+    logging.info(f'The id of l2[1] is: {id(list_param[1])}')
+
 
 def main():
     
     l = [1,2,3,4,5]
+    l2 = [1,[1,2]]
     t = (1,2,3,4,5)
     logging.info(f'The id of l is: {id(l)}')
     logging.info(f'The id of t is: {id(t)}')
+
+    logging.info('--test-1--')
     afunc(l,t)
     logging.info(f'l is {l}')
     logging.info(f't is {t}')
     logging.info(f'The id of l is: {id(l)}')
     logging.info(f'The id of t is: {id(t)}')
+
+    logging.info('--test-2--')
     afunc2(l,t)
     logging.info(f'l is {l}')
     logging.info(f't is {t}')
     logging.info(f'The id of l is: {id(l)}')
     logging.info(f'The id of t is: {id(t)}')
+
+    logging.info('--test-3--')
     afunc2(l[:],t) # * This time a copy is received and not l itself, the function cannot change the value
-    logging.info(f'l is {l}')
+    logging.info(f'l2 is {l2}')
     logging.info(f't is {t}')
-    logging.info(f'The id of l is: {id(l)}')
+    logging.info(f'The id of l2 is: {id(l2)}')
+    logging.info(f'The id of t is: {id(t)}')
+
+    logging.info('--test-4--')
+    afunc3(l2,t) 
+    logging.info(f'l2 is {l2}')
+    logging.info(f't is {t}')
+    logging.info(f'The id of l2 is: {id(l2)}')
+    logging.info(f'The id of t is: {id(t)}')
+
+    logging.info('--test-5--')
+    afunc3(l2[:],t) # * This is only a shallow copy though
+    logging.info(f'l2 is {l2}')
+    logging.info(f't is {t}')
+    logging.info(f'The id of l2 is: {id(l2)}')
+    logging.info(f'The id of l2[1] is: {id(l2[1])}')
     logging.info(f'The id of t is: {id(t)}')
 
 if __name__ == '__main__':
