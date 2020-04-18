819ad16c3ba00ff0573e992c843ce812fe993121
Moyewa Odiase <moyeodiase@hotmail.co.uk>
2020-04-18 10:06:11 +0100

    Mock spec_set example

--- unittest/mock/mock-spec.py
+++ unittest/mock/mock-spec.py
@@ -11,8 +11,14 @@ class SomeClass:
 
 if __name__ == "__main__":
     instance = SomeClass()
-    mock = mock.Mock(spec=instance)
-    mock.a  # * no problem
-    mock.b  # * no problem
-    mock.c  # * no problem
-    # mock.d  # ! This will raise an AttributeError because SomeClass does not define an attribute 'd'
+    mock_obj = mock.Mock(spec=instance)
+    mock_obj.a  # * no problem
+    mock_obj.b  # * no problem
+    mock_obj.c  # * no problem
+    # mock_obj.d  # ! This will raise an AttributeError because SomeClass does not define an attribute 'd'
+    mock_obj.e = 2  # * But setting is still allowed
+
+    mock_obj2 = mock.Mock(
+        spec_set=instance
+    )  # * Spec only prevents getting, to also prevent setting use spec_set
+    # mock_obj2.e = 2  # ! Now this is not allowed
