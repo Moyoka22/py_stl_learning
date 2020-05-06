d4e6ec0000fa399b1c9f7ad3d08a1047b5ed81e3
Moyewa Odiase <moyeodiase@hotmail.co.uk>
2020-05-06 21:05:51 +0100

    Added additional islice example; realised islice is short for iterator slice

--- itertools/itertools-islice.py
+++ itertools/itertools-islice.py
@@ -1,8 +1,15 @@
-from functools import islice
+from itertools import islice
 
+# islice = iterator slice
 
 from string import ascii_lowercase
+
 # ! Not that it is [5,13) but the first index is 0
-someletters = islice(ascii_lowercase, 5, 13)
-for l in someletters:
+some_letters = islice(ascii_lowercase, 5, 13)
+for l in some_letters:
     print(l)  # * Prints 'f g h i j k l m'
+
+# ! islice will automatically stop if the iterator is exhausted
+some_more_letters = islice(ascii_lowercase, 0, 100, 10)  # * Can specify a step
+for l in some_more_letters:
+    print(l)  # * Prints 'a k u'
